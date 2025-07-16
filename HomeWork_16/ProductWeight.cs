d651fc9b672538905492a71aede3bcc359b8e033
Mariam Pankelashvili <mari.pankelashvili@gmail.com>
2025-07-16 18:51:07 +0400

    Homework - Lesson 16

---
+++ HomeWork_16/ProductWeight.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_16
+{
+    public class ProductWeight : IComparable<ProductWeight>
+    {
+        public string Name { get; private set; }
+        public Unit UnitType { get; private set; }
+        public decimal Weight { get; private set; }
+
+        public ProductWeight(string name, Unit unitType, decimal weight)
+        {
+            Name = name;
+            UnitType = unitType;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} Weight - {Weight} {UnitType}";
+        }
+
+        public void UnitCheck(ProductWeight anotherOne)
+        {
+            if (this.UnitType != anotherOne.UnitType)
+            {
+                throw new ArgumentException("Please compare products with the same unit types");
+            }
+        }
+
+        public int CompareTo(ProductWeight anotherOne)
+        {
+            UnitCheck(anotherOne);
+            return this.Weight.CompareTo(anotherOne.Weight);
+        }
+
+        public static ProductWeight operator +(ProductWeight a, ProductWeight b)
+        {
+            a.UnitCheck(b);
+            return new ProductWeight($"{a.Name}+{b.Name}", a.UnitType, a.Weight + b.Weight);
+        }
+
+        public static bool operator == (ProductWeight a, ProductWeight b)
+        {
+            if(a.Weight == b.Weight && a.UnitType == b.UnitType)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(ProductWeight a, ProductWeight b) {
+
+            if (a.Weight != b.Weight || a.UnitType != b.UnitType)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator <(ProductWeight a, ProductWeight b)
+        {
+            a.UnitCheck(b);
+            return a.Weight < b.Weight;
+        }
+
+        public static bool operator >(ProductWeight a, ProductWeight b)
+        {
+            a.UnitCheck(b);
+            return a.Weight > b.Weight;
+        }
+
+    }
+}
---
+++ HomeWork_16/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HomeWork_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductWeight product1 = new ProductWeight("product1", Unit.kg,10);
+            ProductWeight product2 = new ProductWeight("product2", Unit.gr, 100);
+            ProductWeight product3 = new ProductWeight("product3", Unit.kg, 45);
+            ProductWeight product4 = new ProductWeight("product4", Unit.tn, 50);
+            ProductWeight product5 = new ProductWeight("product5", Unit.gr, 240);
+            ProductWeight product6 = new ProductWeight("product6", Unit.kg, 46);
+
+            Console.WriteLine(product1);
+            Console.WriteLine(product2);
+            Console.WriteLine(product3);
+            Console.WriteLine(product4);
+            Console.WriteLine(product5);
+            Console.WriteLine(product6);
+
+            Console.WriteLine(product2.CompareTo(product5));
+
+            Console.WriteLine(product1+product3);
+            Console.WriteLine(product1 < product3);
+
+        }
+    }
+}
